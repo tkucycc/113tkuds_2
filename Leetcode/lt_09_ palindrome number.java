16166133af328d0288ff779bb714c4a2205a21b9
412631086 <145464164+tkucycc@users.noreply.github.com>
2025-09-03 08:19:58 +0800

    Create lt_09_ palindrome number.java

---
+++ Leetcode/lt_09_ palindrome number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        String s = Integer.toString(x);
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
