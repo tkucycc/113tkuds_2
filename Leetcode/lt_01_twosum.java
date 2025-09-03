ef1a68acb8a3d2b3ec7c116c4a56d4dffd403a97
412631086 <145464164+tkucycc@users.noreply.github.com>
2025-09-03 08:35:58 +0800

    Update lt_01_twosum.java

--- Leetcode/lt_01_twosum.java
+++ Leetcode/lt_01_twosum.java
@@ -1,20 +1,23 @@
-import java.util.HashMap;
+import java.util.*;
 
-class Solution {
+class L01_TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
+        Map<Integer, Integer> map = new HashMap<>(); // 值 -> 索引
         for (int i = 0; i < nums.length; i++) {
-            int diff = target - nums[i];
-            
-            if (map.containsKey(diff)) {
-                return new int[] { map.get(diff), i };
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[]{ map.get(need), i };
             }
-            
             map.put(nums[i], i);
         }
-        
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[0]; // 題目保證有解，實務上不會到這
     }
 }
+
+/*
+解題思路：
+- 以 HashMap 記錄「數值 -> 索引」；走訪陣列時檢查 target - nums[i] 是否已出現。
+- 找到就回傳兩索引；沒找到就把目前值存入表。
+- 時間 O(n)，空間 O(n)。
+*/
 
